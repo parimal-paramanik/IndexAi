f660ed930e54e7d8331a8fc901ccb54e1fad80aa
Parimal Paramanik <115460386+parimal-paramanik@users.noreply.github.com>
2024-04-23 02:18:24 +0530

    Update Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -5,7 +5,7 @@ const Contact = () => {
     <div style={{ width: '640px', height: '700px', margin: 'auto' }}>
       <iframe
         title="Contact Form"
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdKdLJ4NIrpwhUFuCLPN6Z8bMy-ZJ0dMDBnw4ovYPzX_aINzA/viewform?embedded=true"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfZ7uRKKapiCzsPMl-6igbIg_jnhRFtOlnFx8kxm8u8DyR3Qg/viewform?embedded=true"
         width="100%"
         height="100%"
       >
